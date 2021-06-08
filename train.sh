b6e03e91ae15535efaba1c32c2c6634c647dc556
Samuel Lo <systemx637@gmail.com>
2021-06-09 00:12:36 +0100

    python 3

--- train.sh
+++ train.sh
@@ -10,6 +10,6 @@ source /home/s2122322/anaconda2/bin/activate
 conda activate dctts
 echo Conda environment dctts activated
 
-python2 prepo.py
-python2 train.py 1
-python2 train.py 2
+python prepo.py
+python train.py 1
+python train.py 2
